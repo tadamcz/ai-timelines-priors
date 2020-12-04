3eb2d78f99a1e2a5de150eb40b5fb3a17ce493a8
tmkadamcz <tmkadamcz@gmail.com>
2020-12-04 13:40:35 -0800

    verify laplace with 0.5 virtual == (bayes with jeffreys prior)

--- main.py
+++ main.py
@@ -1,48 +1,60 @@
-from scipy import integrate
+from scipy import integrate, stats
 import numpy as np
 import matplotlib.pyplot as plt
 from pytest import approx
 
-uniform = lambda x:1 # a constant pdf equal to 1 corresponds to the uniform prior
+uniform = lambda x: 1  # a constant PDF equal to 1 corresponds to the uniform prior
+jeffreys = stats.beta(a=1/2, b=1/2).pdf  # Jeffreys prior PDF is a beta(1/2,1/2). See https://en.wikipedia.org/wiki/Jeffreys_prior#Bernoulli_trial
 
-def laplace(trials,failures):
+
+def laplace(trials, failures, virtual_successes=1, virtual_failures=1):
 	successes = trials - failures
-	virtual_sucesses = 1
-	virtual_failures = 1
-	next_trial_p = (virtual_sucesses+successes)/(trials+virtual_sucesses+virtual_failures)
+	next_trial_p = (virtual_successes + successes) / (trials + virtual_successes + virtual_failures)
 	return next_trial_p
 
-def forecast_laplace(failures=2020-1956,forecast_years=16):
+
+def forecast_laplace(failures, forecast_years, virtual_successes=1, virtual_failures=1):
 	p_failure_by_target = 1
 	for i in range(forecast_years):
-		p_failure = 1-laplace(failures,failures)
-		p_failure_by_target = p_failure_by_target*p_failure
+		p_failure = 1 - laplace(failures, failures, virtual_successes, virtual_failures)
+		p_failure_by_target = p_failure_by_target * p_failure
 		failures += 1
-	p_success_by_target = 1-p_failure_by_target
+	p_success_by_target = 1 - p_failure_by_target
 	return p_success_by_target
 
-def _noAIupdate_bayes_single(p_density):
-	p_density_unnormalized = lambda x: p_density(x)*(1-x)
-	normalization_constant = integrate.quad(p_density_unnormalized,0,1)[0]
-	return lambda x: p_density_unnormalized(x)/normalization_constant
 
-def noAIupdate_bayes(failures,prior):
+def _noAIupdate_bayes_single(p_density):
+	p_density_unnormalized = lambda x: p_density(x) * (1 - x)
+	normalization_constant = integrate.quad(p_density_unnormalized, 0, 1)[0]
+	return lambda x: p_density_unnormalized(x) / normalization_constant
+
+
+def noAIupdate_bayes(failures, prior):
 	p_density = prior
 	plot_domain = np.linspace(0, 1, 50)
 	for i in range(failures):
 		p_density = _noAIupdate_bayes_single(p_density)
-		# plt.plot(plot_domain,p_density(plot_domain))
-		# plt.show()
+	# plt.plot(plot_domain,p_density(plot_domain))
+	# plt.show()
 	return p_density
 
-def forecast_bayes(failures,prior,forecast_years):
+
+def forecast_bayes(failures, prior, forecast_years):
 	p_failure_by_target = 1
 	for i in range(forecast_years):
-		density = noAIupdate_bayes(failures,prior)
-		p_failure = 1-integrate.quad(lambda x: density(x) * x, 0, 1)[0]
-		p_failure_by_target = p_failure_by_target*p_failure
+		density = noAIupdate_bayes(failures, prior)
+		p_failure = 1 - integrate.quad(lambda x: density(x) * x, 0, 1)[0]
+		p_failure_by_target = p_failure_by_target * p_failure
 		failures += 1
-	p_success_by_target = 1-p_failure_by_target
+	p_success_by_target = 1 - p_failure_by_target
 	return p_success_by_target
 
-assert approx(forecast_bayes(failures=64,prior=uniform,forecast_years=16)) == forecast_laplace(failures=64,forecast_years=16)
+# Verify that Bayes with uniform prior corresponds to Laplace
+assert approx(
+	forecast_bayes(failures=5, prior=uniform, forecast_years=3)) == \
+	forecast_laplace(failures=5, forecast_years=3)
+
+# Verify that Bayes with Jeffrey's prior corresponds to modified laplace with 0.5 virtual successes and failures
+assert approx(
+	forecast_bayes(failures=5, prior=jeffreys, forecast_years=3)) == \
+	forecast_laplace(failures=5, forecast_years=3, virtual_failures=0.5, virtual_successes=0.5)
