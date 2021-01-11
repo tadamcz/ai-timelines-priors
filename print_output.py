b88b422a4a6bd9607c42cc1da9a675db28b8900b
tadamcz <tmkadamcz@gmail.com>
2021-01-11 13:57:18 -0800

    switch from assertions.py to just print_output.py. Client wants me to update values in document directly.

---
+++ print_output.py
@@ -0,0 +1,10 @@
+import sections
+
+sections.section4_2()
+sections.section5_2_2()
+sections.section5_2_3_t1()
+sections.section5_2_3_t2_left()
+sections.section5_2_3_t2_right()
+sections.section5_3_t1()
+sections.section5_3_t2()
+sections.section6_1_2()
